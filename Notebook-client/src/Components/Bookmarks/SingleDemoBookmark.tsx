6c46848fe7721a4224454a1ab3d41771f47dbeba
Sadmaney Yeasar <1730034@iub.edu.bd>
2022-04-20 06:53:16 +0600

    done

--- Notebook-client/src/Components/Bookmarks/SingleDemoBookmark.tsx
+++ Notebook-client/src/Components/Bookmarks/SingleDemoBookmark.tsx
@@ -7,10 +7,6 @@ const SingleDemoBookmark = ({
     setIsModified,
     isModified
 }) => {
-    // console.log(bookmark)
-    const [isUpdate, setIsUpdate] = useState <boolean> (false)
-    const [hello, setHello] = useState <string> ("")
-    
   return (
      <>
       <ShowSingleBookmarks bookmark={bookmark} setIsModified = {setIsModified} isModified = {isModified}/> 
--- Notebook-client/src/Components/Bookmarks/UpdateModal/UpdateModal.tsx
+++ Notebook-client/src/Components/Bookmarks/UpdateModal/UpdateModal.tsx
@@ -1,33 +1,56 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Form, Modal } from "react-bootstrap";
+import { Button, Form, Modal, Spinner } from "react-bootstrap";
 import "./UpdateModal.css";
-const UpdateModal = ({ show, handleClose, bookmark }) => {
-  // const { title, category, description } = bookmark;
+const UpdateModal = ({ show, handleClose, bookmark, isClick,setIsModified, isModified  }) => {
+  const [isSubmitButtonCLick, setIsSubmitButtonClick] = useState <boolean>(false)
+  const [isSuccessfulUpdated, setSuccessfulUpdated] = useState  <boolean>(false)
+  const [isUpdateProcessComplete, setIsUpdateProcessComplete] = useState  <boolean> (false)
   const [bookmarkData, setBookmarkData] = useState({
     title: "",
     description: "",
     category: "",
   });
-  const updateHandler = (e) => {
+  const updateHandler = async (e): Promise <void> => {
     e.preventDefault();
-    axios
-      .put(`http://localhost:4300/form/update/${bookmark._id}`, bookmarkData)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+    setIsSubmitButtonClick (true);
   }
 
+  //info update part
+  useEffect (() => {
+    if (isSubmitButtonCLick) {
+      (async ():Promise <void> => {
+          const {data} = await axios.put(`http://localhost:4300/form/update/${bookmark._id}`, bookmarkData)
+          if (data.status == 202) {
+            setIsModified (!isModified)
+            setSuccessfulUpdated (true);
+          }
+          setIsUpdateProcessComplete (true);
+        })()
+    }
+  }, [isSubmitButtonCLick])
+
+  //when update process complete it will hide the modal
+  useEffect (() => {
+    if (isUpdateProcessComplete) {
+      handleClose();
+    }
+  }, [isUpdateProcessComplete])
+
+  //store the existed bookmark data into local state so that it will show as form default value
   useEffect(() => {
-    (async () => {
-      // const { data } = await axios.get(
-      //   `http://localhost:4300/form/bookmark/${bookmark._id}`
-      // );
-    })();
-  }, []);
+    setIsSubmitButtonClick (false) 
+    setSuccessfulUpdated (false)
+    setBookmarkData (
+      {
+        title: bookmark.title,
+        description: bookmark.description,
+        category: bookmark.category
+      }
+    )
+  }, [isClick]);
+
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
@@ -70,19 +93,6 @@ const UpdateModal = ({ show, handleClose, bookmark }) => {
             />
           </Form.Group>
 
-          {/* bookmark link */}
-          {/* <Form.Group className="mb-3" controlId="">
-            <Form.Label>Link</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter email"
-              value={bookmark.link}
-              onChange={(e) =>
-                setBookmarkData({ ...bookmarkData, link: e.target.value })
-              }
-            />
-          </Form.Group> */}
-
           {/* bookmark category */}
           <Form.Group className="mb-3" controlId="">
             <Form.Label>Category</Form.Label>
@@ -99,10 +109,34 @@ const UpdateModal = ({ show, handleClose, bookmark }) => {
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
-          Close
+          Close 
         </Button>
-        <Button variant="primary" onClick={updateHandler}>
-          Save Changes
+        <Button variant="primary" onClick={updateHandler} style = {{width: "35%", overflow: "hidden"}}>
+          {
+            isSubmitButtonCLick  //when user click the update button that time a spinner will show and the text will be remove
+            ?
+            <>
+              {
+                isUpdateProcessComplete  //check that is update process is running or complete if not then it will show a spinner
+                ?
+                <>
+                  {
+                    isSuccessfulUpdated //if successfully updated then it will show a right sign otherwise it will show cross sign
+                    ?
+                     <FontAwesomeIcon icon= {["fas", "check-double"]} />
+                    :
+                     <FontAwesomeIcon icon= {["fas", "xmark"]} />
+                  }
+                </>
+                :
+                <Spinner animation="grow" />
+              }
+            </>
+            :
+            <>
+              Save Changes
+            </>
+          }
         </Button>
       </Modal.Footer>
     </Modal>
